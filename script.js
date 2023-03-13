2d84ff97021a7a40202a7e63a976594a26ea3a29
ilia-io <likeapu@gmail.com>
2023-03-13 19:05:08 +0300

    feat: basic styles

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('hello');
+const form = document.querySelector('.form');
+const inputName = form.elements.name;
+const inputText = form.elements.text;
+const inputDate = form.elements.date;
+//const inputBtn = form.elements.btn;
